e011686d1de1c16bc651ff5edff31f484a05d620
Unknown-Stryker <leehojin6580@gmail.com>
2023-07-09 12:02:10 +0900

    Update heap_utilization.hpp

--- Frogman-Engine-Core-Library/FE/core/heap_utilization.hpp
+++ Frogman-Engine-Core-Library/FE/core/heap_utilization.hpp
@@ -10,14 +10,14 @@
 BEGIN_NAMESPACE(FE)
 
 
-template<typename T>
+template<typename T, class alignment>
 class heap_utilization;
 
 template<typename T>
 class new_delete_allocator;
 
-template <typename T>
-class scalable_allocator;
+template <typename T, class alignment>
+class scalable_aligned_allocator;
 
 template <typename T>
 class cache_aligned_allocator;
@@ -36,11 +36,11 @@ protected:
 };
 
 
-template<typename U>
+template<typename U, class alignment = align_8bytes>
 class heap_utilization final : public heap_memory_tracker
 {
 	friend class new_delete_allocator<U>;
-	friend class scalable_allocator<U>;
+	friend class scalable_aligned_allocator<U, alignment>;
 	friend class cache_aligned_allocator<U>;
 
 private:
@@ -113,47 +113,48 @@ public:
 	}
 
 public:
-	template<typename T>
+	template<typename T, class alignment>
 	friend _FORCE_INLINE_ T* trackable_calloc(length_t count_p, size_t bytes_p) noexcept;
 
-	template<typename T>
+	template<typename T, class alignment>
 	friend _FORCE_INLINE_ void trackable_free(T* const memblock_ptrc_p, length_t count_p, size_t bytes_p) noexcept;
 
-	template<typename T>
+	template<typename T, class alignment>
 	friend _FORCE_INLINE_ T* trackable_realloc(T* const memblock_ptrc_p, length_t prev_length_p, size_t prev_bytes_p, length_t new_length_p, size_t new_bytes_p) noexcept;
 };
 
 
-template<typename T>
-::std::atomic_size_t FE::heap_utilization<T>::s_global_total_bytes_by_type = 0;
+template<typename T, class alignment>
+::std::atomic_size_t FE::heap_utilization<T, alignment>::s_global_total_bytes_by_type = 0;
 
-template<typename T>
-thread_local var::size_t FE::heap_utilization<T>::tl_s_thread_local_total_bytes_by_type = 0;
+template<typename T, class alignment>
+thread_local var::size_t FE::heap_utilization<T, alignment>::tl_s_thread_local_total_bytes_by_type = 0;
 
 
-template<typename T>
+template<typename T, class alignment = align_8bytes>
 _NODISCARD_ _FORCE_INLINE_ T* trackable_calloc(length_t count_p, size_t bytes_p) noexcept
 {
 #if _ENABLE_MEMORY_TRACKER_ == true
 	::FE::heap_utilization<T>::add(count_p * bytes_p);
 #endif
-	T* l_result_ptr = (T*)::scalable_calloc(count_p, bytes_p);
+	T* l_result_ptr = (T*)::scalable_aligned_malloc(count_p * bytes_p, alignment::s_size);
+	::FE::memset_s(l_result_ptr, _NULL_, count_p, bytes_p);
 	FE_ASSERT(l_result_ptr == nullptr, "UNRECOVERABLE CRITICAL ERROR!: l_result_ptr is nullptr. Failed to allocate memory from scalable_calloc()", _ASSERTED_LOCATION_);
 	return l_result_ptr;
 }
 
 
-template<typename T>
+template<typename T, class alignment = align_8bytes>
 _FORCE_INLINE_ void trackable_free(T* const memblock_ptrc_p, length_t count_p, size_t bytes_p) noexcept
 {
 #if _ENABLE_MEMORY_TRACKER_ == true
 	::FE::heap_utilization<T>::sub(count_p * bytes_p);
 #endif 
-	::scalable_free(memblock_ptrc_p);
+	::scalable_aligned_free(memblock_ptrc_p);
 }
 
 
-template<typename T>
+template<typename T, class alignment = align_8bytes>
 _NODISCARD_ _FORCE_INLINE_ T* trackable_realloc(T* const memblock_ptrc_p, length_t prev_length_p, size_t prev_bytes_p, length_t new_length_p, size_t new_bytes_p) noexcept
 {
 #if _ENABLE_MEMORY_TRACKER_ == true
@@ -161,16 +162,17 @@ _NODISCARD_ _FORCE_INLINE_ T* trackable_realloc(T* const memblock_ptrc_p, length
 	::FE::heap_utilization<T>::s_global_total_bytes_by_type::add(new_length_p * new_bytes_p);
 #endif
 
-	T* l_realloc_result_ptr = (T*)::scalable_realloc(memblock_ptrc_p, new_bytes_p * new_length_p);
+	T* l_realloc_result_ptr = (T*)::scalable_aligned_realloc(memblock_ptrc_p, new_bytes_p * new_length_p, alignment::s_size);
 
 	if (l_realloc_result_ptr == nullptr) _UNLIKELY_
 	{
-		l_realloc_result_ptr = (T*)::scalable_calloc(new_length_p, new_bytes_p);
+		l_realloc_result_ptr = (T*)::scalable_aligned_malloc(new_length_p * new_bytes_p, alignment::s_size);
+		::FE::memset_s(l_realloc_result_ptr, _NULL_, new_length_p, new_bytes_p);
 
 		FE_ASSERT(l_realloc_result_ptr == nullptr, "CRITICAL ERROR: Failed to re-allocate memory", _ASSERTED_LOCATION_);
 
 		::FE::memcpy_s(l_realloc_result_ptr, new_length_p, new_bytes_p, memblock_ptrc_p, prev_length_p, prev_bytes_p);
-		::scalable_free(memblock_ptrc_p);
+		::scalable_aligned_free(memblock_ptrc_p);
 	}
 
 	return l_realloc_result_ptr;
