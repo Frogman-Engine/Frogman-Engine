a0bd5e2ae72d6024eba2b879cb3a4772eb5e50c5
Unknown-Stryker <leehojin6580@gmail.com>
2023-07-14 01:46:18 +0900

    Update stack.hxx

--- Frogman-Engine-Core-Library/FE/core/containers/stack.hxx
+++ Frogman-Engine-Core-Library/FE/core/containers/stack.hxx
@@ -1,5 +1,5 @@
-﻿#ifndef _FE_CORE_CONCURRENT_STACK_HXX_
-#define _FE_CORE_CONCURRENT_STACK_HXX_
+﻿#ifndef _FE_CORE_STACK_HXX_
+#define _FE_CORE_STACK_HXX_
 // Copyright © from 2023 to current, UNKNOWN STRYKER. All Rights Reserved.
 #include <FE/core/prerequisite_symbols.h>
 #include <FE/core/iterator.hxx>
