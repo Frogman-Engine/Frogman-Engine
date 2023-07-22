cc22bd307c388a5a56b3efb94519a0eae9683c74
Unknown-Stryker <leehojin6580@gmail.com>
2023-07-22 15:33:43 +0900

    Linux project has a few linker issues
    This issue will be fixed within a few hours.
    I need to get some sleep since I was working roughly ten hours straight.

--- Frogman-Engine-Core-Library/FE/core/private/heap_memory_tracker.cpp
+++ Frogman-Engine-Core-Library/FE/core/private/heap_memory_tracker.cpp
@@ -7,3 +7,7 @@ std::atomic_size_t FE::heap_memory_tracker_base::s_global_total_bytes = 0;
 thread_local FE::var::size_t FE::heap_memory_tracker_base::tl_s_thread_local_total_bytes = 0;
 
 FE::lazy_const<FE::var::size_t> FE::internal::heap_memory_tracker_initialization_argument::s_output_stream_buffer_size;
+
+thread_local std::ofstream FE::heap_memory_tracker_base::tl_s_utilized_heap_memory_output_stream;
+thread_local std::string FE::heap_memory_tracker_base::tl_s_utilized_heap_memory_output_stream_buffer;
+thread_local FE::clock FE::heap_memory_tracker_base::tl_s_clock;
