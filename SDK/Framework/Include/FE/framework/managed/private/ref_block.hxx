313bd4f0f79059a3d4793a424460cacff5ca6b0c
Unknown-Stryker <leehojin6580@gmail.com>
2024-11-06 16:00:18 +0900

    Changed the license to apache 2.0

--- SDK/Framework/Include/FE/framework/managed/private/ref_block.hxx
+++ SDK/Framework/Include/FE/framework/managed/private/ref_block.hxx
@@ -1,6 +1,20 @@
 ﻿#ifndef _FE_CORE_MANAGED_REF_BLOCK_HXX_
 #define _FE_CORE_MANAGED_REF_BLOCK_HXX_
-// Copyright © from 2023 to current, UNKNOWN STRYKER. All Rights Reserved.
+/*
+Copyright © from 2022 to present, UNKNOWN STRYKER. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 #include <FE/prerequisites.h>
 
 
